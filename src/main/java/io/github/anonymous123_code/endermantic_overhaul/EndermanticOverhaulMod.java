b5805fa378875ac0ecd17615c45ff93412e572fe
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-03-11 20:05:35 +0100

    More Setup + first EndForceConcentrator Version

---
+++ src/main/java/io/github/anonymous123_code/endermantic_overhaul/EndermanticOverhaulMod.java
@@ -0,0 +1,33 @@
+package io.github.anonymous123_code.endermantic_overhaul;
+
+import io.github.anonymous123_code.endermantic_overhaul.blocks.EnderForceConcentratorBlock;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
+import net.minecraft.block.Block;
+import net.minecraft.block.Material;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class EndermanticOverhaulMod implements ModInitializer {
+	// This logger is used to write text to the console and the log file.
+	// It is considered best practice to use your mod id as the logger's name.
+	// That way, it's clear which mod wrote info, warnings, and errors.
+	public static final Logger LOGGER = LoggerFactory.getLogger("endermantic_overhaul");
+	public static final Block ENDER_FORCE_CONCENTRATOR = new EnderForceConcentratorBlock(FabricBlockSettings.of(Material.METAL).requiresTool().strength(4.0f));
+
+	@Override
+	public void onInitialize() {
+		// This code runs as soon as Minecraft is in a mod-load-ready state.
+		// However, some things (like resources) may still be uninitialized.
+		// Proceed with mild caution.
+		Registry.register(Registry.BLOCK, new Identifier("endermantic_overhaul", "ender_force_concentrator"), ENDER_FORCE_CONCENTRATOR);
+		Registry.register(Registry.ITEM, new Identifier("endermantic_overhaul", "ender_force_concentrator"), new BlockItem(ENDER_FORCE_CONCENTRATOR, new FabricItemSettings().group(ItemGroup.MISC)));
+
+		LOGGER.info("Hello Fabric world!");
+	}
+}
---
+++ src/main/java/io/github/anonymous123_code/endermantic_overhaul/blocks/EnderForceConcentratorBlock.java
@@ -0,0 +1,64 @@
+package io.github.anonymous123_code.endermantic_overhaul.blocks;
+
+
+import net.minecraft.block.Block;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.FacingBlock;
+import net.minecraft.entity.mob.EndermanEntity;
+import net.minecraft.item.ItemPlacementContext;
+import net.minecraft.state.StateManager;
+import net.minecraft.state.property.BooleanProperty;
+import net.minecraft.state.property.Properties;
+import net.minecraft.util.BlockMirror;
+import net.minecraft.util.BlockRotation;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.math.Direction;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+
+public class EnderForceConcentratorBlock extends FacingBlock {
+    public static final BooleanProperty POWERED = Properties.POWERED;
+
+    public EnderForceConcentratorBlock(Settings settings) {
+        super(settings);
+        setDefaultState(getStateManager().getDefaultState().with(FACING, Direction.UP).with(POWERED, false));
+    }
+
+    @Override
+    public BlockState getPlacementState(ItemPlacementContext ctx) {
+        return this.getDefaultState().with(FACING, ctx.getPlayerLookDirection().getOpposite());
+    }
+
+    @Override
+    protected void appendProperties(StateManager.Builder<Block, BlockState> builder) {
+        super.appendProperties(builder);
+        builder.add(FACING, POWERED);
+    }
+
+    public BlockState rotate(BlockState state, BlockRotation rotation) {
+        return state.with(FACING, rotation.rotate(state.get(FACING)));
+    }
+
+    public BlockState mirror(BlockState state, BlockMirror mirror) {
+        return state.rotate(mirror.getRotation(state.get(FACING)));
+    }
+
+    public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
+        if (!world.isClient) {
+            boolean powered = world.isReceivingRedstonePower(pos);
+            if (powered != state.get(POWERED)) {
+                if (powered) {
+                    BlockPos facingBlock = pos.offset(state.get(FACING), 1);
+                    List<EndermanEntity> endermen = world.getEntitiesByClass(EndermanEntity.class, new Box(facingBlock.add(8, 8, 8), facingBlock.add(-8, -8, -8)), endermanEntity -> facingBlock.isWithinDistance(endermanEntity.getPos(), 8) && !endermanEntity.isAiDisabled() && !endermanEntity.isDead());
+                    endermen.forEach((EndermanEntity e) -> e.teleport(facingBlock.getX() + 0.5, facingBlock.getY(), facingBlock.getZ() + 0.5));
+                }
+
+                world.setBlockState(pos, state.with(POWERED, powered));
+            }
+
+        }
+    }
+}
