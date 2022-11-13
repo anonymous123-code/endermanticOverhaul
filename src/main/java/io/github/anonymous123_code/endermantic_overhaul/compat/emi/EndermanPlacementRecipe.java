bdfe4020d4ed5a621c877a34de2a795f4d215037
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-11-13 17:57:18 +0100

    Finished Recipe look

--- src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanPlacementRecipe.java
+++ src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanPlacementRecipe.java
@@ -1,11 +1,18 @@
 package io.github.anonymous123_code.endermantic_overhaul.compat.emi;
 
+import com.ibm.icu.text.BidiTransform;
+import dev.emi.emi.EmiPort;
+import dev.emi.emi.api.EmiApi;
 import dev.emi.emi.api.recipe.EmiRecipe;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.render.EmiTexture;
 import dev.emi.emi.api.stack.EmiIngredient;
 import dev.emi.emi.api.stack.EmiStack;
 import dev.emi.emi.api.widget.WidgetHolder;
 import io.github.anonymous123_code.endermantic_overhaul.EndermanticOverhaulMod;
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.text.OrderedText;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import org.jetbrains.annotations.Nullable;
 
@@ -13,6 +20,7 @@ import java.util.ArrayList;
 import java.util.List;
 //TODO: Unhardcode
 public class EndermanPlacementRecipe implements EmiRecipe {
+    private OrderedText text = Text.translatable("predicate.endermantic_overhaul.negative_ender_force_emitter_crafting").asOrderedText();
 
     @Override
     public EmiRecipeCategory getCategory() {
@@ -40,16 +48,19 @@ public class EndermanPlacementRecipe implements EmiRecipe {
 
     @Override
     public int getDisplayWidth() {
-        return 0;
+        return Math.max(MinecraftClient.getInstance().textRenderer.getWidth(this.text)+4, EmiTexture.EMPTY_ARROW.width + 68);
     }
 
     @Override
     public int getDisplayHeight() {
-        return 0;
+        return 32;
     }
 
     @Override
     public void addWidgets(WidgetHolder widgets) {
-
+        widgets.addSlot(getInputs().get(0), getDisplayWidth()/2 - EmiTexture.EMPTY_ARROW.width/2 - 32,0);
+        widgets.addTexture(EmiTexture.EMPTY_ARROW, getDisplayWidth()/2 - EmiTexture.EMPTY_ARROW.width/2 - 1,1);
+        widgets.addSlot(getOutputs().get(0), getDisplayWidth()/2 + EmiTexture.EMPTY_ARROW.width/2 + 16,0);
+        widgets.addText(text,  getDisplayWidth()/2 - MinecraftClient.getInstance().textRenderer.getWidth(this.text)/2, 22, -1, true);
     }
 }
--- src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanticOverhaulEmiPlugin.java
+++ src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanticOverhaulEmiPlugin.java
@@ -5,12 +5,14 @@ import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
 import dev.emi.emi.api.stack.EmiStack;
 import net.minecraft.item.Items;
+import net.minecraft.text.Style;
+import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 
 public class EndermanticOverhaulEmiPlugin implements EmiPlugin {
-    public static final EmiStack ENDERMAN_PLACEMENT_WORKSTATION = EmiStack.of(Items.ENDERMAN_SPAWN_EGG);
+    public static final EmiStack ENDERMAN_PLACEMENT_WORKSTATION = EmiStack.of(Items.ENDERMAN_SPAWN_EGG.getDefaultStack().setCustomName(Text.translatable("entity.minecraft.enderman").setStyle(Style.EMPTY.withItalic(false))));
     public static final EmiRecipeCategory ENDERMAN_PLACEMENT_CATEGORY
-            = new EmiRecipeCategory(new Identifier("endermantic_overhaul", "enderman_placement_recipe"), ENDERMAN_PLACEMENT_WORKSTATION);
+            = new EmiRecipeCategory(new Identifier("endermantic_overhaul", "enderman_placement"), ENDERMAN_PLACEMENT_WORKSTATION);
 
     @Override
     public void register(EmiRegistry registry) {
