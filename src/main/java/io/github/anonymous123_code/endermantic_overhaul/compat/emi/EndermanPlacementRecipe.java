aee123a1bd42767ee9bb6bfbcd74c4fdaff4cb79
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-11-13 15:46:20 +0100

    Add todo

--- src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanPlacementRecipe.java
+++ src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanPlacementRecipe.java
@@ -11,7 +11,7 @@ import org.jetbrains.annotations.Nullable;
 
 import java.util.ArrayList;
 import java.util.List;
-
+//TODO: Unhardcode
 public class EndermanPlacementRecipe implements EmiRecipe {
 
     @Override
