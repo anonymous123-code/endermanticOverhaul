4c6de35ce481cad90d1fcbc372b5acbd8848f4df
anonymous123-code <61744596+anonymous123-code@users.noreply.github.com>
2022-11-13 18:17:48 +0100

    Change simplified icon to world interaction icon

--- src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanticOverhaulEmiPlugin.java
+++ src/main/java/io/github/anonymous123_code/endermantic_overhaul/compat/emi/EndermanticOverhaulEmiPlugin.java
@@ -1,9 +1,14 @@
 package io.github.anonymous123_code.endermantic_overhaul.compat.emi;
 
+import com.mojang.blaze3d.systems.RenderSystem;
+import dev.emi.emi.EmiRenderHelper;
 import dev.emi.emi.api.EmiPlugin;
 import dev.emi.emi.api.EmiRegistry;
 import dev.emi.emi.api.recipe.EmiRecipeCategory;
+import dev.emi.emi.api.recipe.EmiRecipeSorting;
+import dev.emi.emi.api.recipe.VanillaEmiRecipeCategories;
 import dev.emi.emi.api.stack.EmiStack;
+import net.minecraft.client.gui.DrawableHelper;
 import net.minecraft.item.Items;
 import net.minecraft.text.Style;
 import net.minecraft.text.Text;
@@ -12,7 +17,9 @@ import net.minecraft.util.Identifier;
 public class EndermanticOverhaulEmiPlugin implements EmiPlugin {
     public static final EmiStack ENDERMAN_PLACEMENT_WORKSTATION = EmiStack.of(Items.ENDERMAN_SPAWN_EGG.getDefaultStack().setCustomName(Text.translatable("entity.minecraft.enderman").setStyle(Style.EMPTY.withItalic(false))));
     public static final EmiRecipeCategory ENDERMAN_PLACEMENT_CATEGORY
-            = new EmiRecipeCategory(new Identifier("endermantic_overhaul", "enderman_placement"), ENDERMAN_PLACEMENT_WORKSTATION);
+            = new EmiRecipeCategory(new Identifier("endermantic_overhaul", "enderman_placement"), ENDERMAN_PLACEMENT_WORKSTATION, VanillaEmiRecipeCategories.WORLD_INTERACTION.simplified);
+
+
 
     @Override
     public void register(EmiRegistry registry) {
